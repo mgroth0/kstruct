dcfb1fee68e21c1e51254cd53364cf0625aa5709
mgroth0 <mgroth49@gmail.com>
2023-05-08 20:06:42 -0400

    changed src/main/kotlin/matt/kstruct/bj/cfg/cfg.kt

--- src/main/kotlin/matt/kstruct/bj/cfg/cfg.kt
+++ src/main/kotlin/matt/kstruct/bj/cfg/cfg.kt
@@ -3,35 +3,52 @@ package matt.kstruct.bj.cfg
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
-@Serializable enum class Native {
-  LIB, APP
+@Serializable
+enum class Native {
+    LIB, APP
 }
 
 
-@Serializable class JvmConfig {
-  val exec: JvmExecConfig? = null
+@Serializable
+class JvmConfig {
+    val exec: JvmExecConfig? = null
 }
 
 
-@Serializable sealed interface JvmExecConfig {
-  val useGradleStandardIn: Boolean
+@Serializable
+sealed interface JvmExecConfig {
+    val useGradleStandardIn: Boolean
 }
 
 @SerialName("Typical")
-@Serializable class TypicalJvmExecConfig: JvmExecConfig {
-  override val useGradleStandardIn = false
+@Serializable
+class TypicalJvmExecConfig : JvmExecConfig {
+    override val useGradleStandardIn = false
 }
 
 
 const val STAGING_SUFFIX = "-staging"
 
+/*[[WebAppServer.Companion#DEFAULT_FORCE_TERMINAL]]*/
 @SerialName("Heroku")
-@Serializable class HerokuJvmExecConfig: JvmExecConfig {
-  val herokuAppName: String? = null
-  override val useGradleStandardIn get() = true /*so I can use the terminal during local server testing to kill the server or other stuff*/
-  val herokuStagingAppName get() = herokuAppName?.let { it + STAGING_SUFFIX }
+@Serializable
+class HerokuJvmExecConfig : JvmExecConfig {
+    val herokuAppName: String? = null
+
+
+    override val useGradleStandardIn: Boolean
+        get() {
+
+            /*See comment in WebAppServer.Companion#DEFAULT_FORCE_TERMINA*/
+            return false
+
+            /*so I can use the terminal during local server testing to kill the server or other stuff*/
+            /*return true*/
+        }
+    val herokuStagingAppName get() = herokuAppName?.let { it + STAGING_SUFFIX }
 }
 
-@Serializable enum class JS {
-  LIB, CLIENT
+@Serializable
+enum class JS {
+    LIB, CLIENT
 }
