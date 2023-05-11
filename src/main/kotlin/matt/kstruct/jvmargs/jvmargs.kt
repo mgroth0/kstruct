d4e78a7e5661da0afb2abc482b4da31f41272265
mgroth0 <mgroth49@gmail.com>
2023-05-11 17:17:52 -0400

    changed src/main/kotlin/matt/kstruct/jvmargs/jvmargs.kt

--- src/main/kotlin/matt/kstruct/jvmargs/jvmargs.kt
+++ src/main/kotlin/matt/kstruct/jvmargs/jvmargs.kt
@@ -5,22 +5,35 @@ import matt.kstruct.jvmargs.KJvmArgsSets.FOR_ALL_OTHER_MACHINES_MATT_USES
 import matt.kstruct.jvmargs.KJvmArgsSets.FOR_NEW_MAC
 import matt.model.code.jvm.JvmArgs
 import matt.model.code.sys.NEW_MAC
-import matt.model.data.byte.gigabytes
+import matt.model.data.byte.gibibytes
 
 object KJvmArgsSets {
 
     val FOR_APP_RELEASES = JvmArgs(
-        xmx = 6.gigabytes,
+        xmx = 6.gibibytes,
         enableAssertionsAndCoroutinesDebugMode = false,
     )
 
-    val FOR_ALL_OTHER_MACHINES_MATT_USES = JvmArgs(6.gigabytes)
+    val FOR_ALL_OTHER_MACHINES_MATT_USES = JvmArgs(6.gibibytes)
 
-    val FOR_NEW_MAC = JvmArgs(30.gigabytes)
+    val FOR_NEW_MAC = JvmArgs(30.gibibytes)
+
+
+    val FOR_GRADLE =
+        JvmArgs(
+            xms = 16.gibibytes,
+            xmx = 16.gibibytes,
+            prism = false,
+            enableAssertionsAndCoroutinesDebugMode = false,
+            unlockDiagnosticVmOptions = false,
+            showHiddenFrames = false,
+            useParallelGC = true /*I've seen in multiple places including an official one that this could speed up android builds... but they all say to double check with a profiler*/
+        )
+    val FOR_KOTLIN_DAEMON = FOR_GRADLE.copy(useParallelGC = false)
 
 }
 
 fun jvmArgsForThisMattRuntime() = when (thisMachine) {
     NEW_MAC -> FOR_NEW_MAC
-    else -> FOR_ALL_OTHER_MACHINES_MATT_USES
+    else    -> FOR_ALL_OTHER_MACHINES_MATT_USES
 }
